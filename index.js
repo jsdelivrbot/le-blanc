3ebe692602cf569618020d76cfe40ec3fc285ab2
Alistair <alistair@wobdev.co.uk>
2018-08-20 21:41:48 +0100

    Added getting started

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// Load HTTP module
-var http = require("http");
+var express = require('express')
+var app = express()
 
-// Create HTTP server and listen on port 8000 for requests
-http.createServer(function(request, response) {
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/');
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
