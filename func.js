95f7ebece644339f3831855b9fc83e1476a25a89
Alistair <alistair@wobdev.co.uk>
2018-10-07 02:04:35 +0100

    Small fix for config vars

--- func.js
+++ func.js
@@ -51,8 +51,8 @@ var methods = {
   sendMail : function(toEmail, subject,body){
 
     // =========================EMAIL FUNC - NEEDS TO BE MOVED===========================
-    
-    var api_key = process.env.MAILGUN_APIKEY;
+
+    var api_key = process.env.MAILGUN_API_KEY;
     var domain = process.env.MAILGUN_DOMAIN;
     var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
     var fromAddress = process.env.MAILGUN_FROM;
