ddfdec50f07b50b365fb0e3a5b619d1f45ce7c8a
Alistair <alistair@wobdev.co.uk>
2018-12-01 07:34:03 +0000

    quick fix - oops

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post('/users/add', [
           console.log(err);
         }
         //console.log(result);
-        var emailSubject = 'Secret Santa - Registration' + thisDraw.name;
+        var emailSubject = 'Secret Santa - Registration';
         var emailBody = 'Dear '+newUser.first_name+' <br> Thank you for registering! <br>We will take it from here.<br> Yours sincerely,<br>Secret Santa';
         var emailHTMLBody = 'Dear '+newUser.first_name+' Thank you for registering! We will take it from here. Yours sincerely, Secret Santa';
         func.sendMail(newUser.email, emailSubject, emailBody, emailHTMLBody);
